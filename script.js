6f6681239aca6a43660bd15c48ce6c47f8eb35b7
Sunny Rohra <sunnyr22@gmail.com>
2014-06-10 22:01:24 -0400

    Homework

--- script.js
+++ script.js
@@ -1,5 +1,74 @@
-var t1='I';
-var t2='hope';
-var t3='to';
-var t4='know';
-var t5='javascript.';
+
+var tuesday = function () {
+//variables
+
+var t1='who';
+console.log(t1);
+var t2='let';
+console.log(t2);
+var t3='the';
+console.log(t3);
+var t4='dogs';
+console.log(t4);
+var t5='out';
+console.log(t5);
+
+//splitting
+
+var s='blue moon';
+var ss= split.s(" ");
+console.log (ss);
+
+//concatting
+
+var slipper ='Slippery ';
+console.log (slipper.concat('Man'));
+
+
+//length
+var japanlength = length.Japan;
+console.log (japanlength);
+
+//Array
+
+var array1 = ['I', 'know', 'what', 'you', 'did', 'last', 'summer', 'and', 'winter', 'too' ];
+console.log (array1);
+
+//join
+
+var joinit  = array1.join();
+console.log(joinit);
+
+//push
+
+var summer = array1.push('totally');
+console.log(summer);
+
+//reverse
+
+var back = array1.reverse();
+console.log(back);
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
